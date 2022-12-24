82f2683551b74336128e2def2e5636b56ff12973
davi <davijluna@gmail.com>
2022-12-23 22:23:26 -0300

    test: requisito 1 implementando o teste para a função Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'arroz',
+        'prato fino',
+        '15/08/2010',
+        '15/12/2010',
+        'nz123',
+        'local seco longe da luz',
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == 'arroz'
+    assert product.nome_da_empresa == 'prato fino'
+    assert product.data_de_fabricacao == '15/08/2010'
+    assert product.data_de_validade == '15/12/2010'
+    assert product.numero_de_serie == 'nz123'
+    assert product.instrucoes_de_armazenamento == 'local seco longe da luz'
+# Seu teste deve ser escrito aqui
