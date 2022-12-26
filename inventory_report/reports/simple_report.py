f922a2b6d6f0522f8d68d8ffd41b7f245b5d49ba
davi <davijluna@gmail.com>
2022-12-26 20:34:24 -0300

    feat: requisito 2 e 3 implementando função que gera versão simplificada do relatório

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,18 @@
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(file):
+        data_de_fabricacao = [item["data_de_fabricacao"] for item in file]
+        data_de_fabricacao.sort()
+        data_de_validade = [item["data_de_validade"] for item in file]
+        data_de_validade.sort()
+        empresas_com_mais_produtos = [item["nome_da_empresa"] for item in file]
+        empresa = Counter(empresas_com_mais_produtos).most_common()[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {min(data_de_fabricacao)}\n"
+            f"Data de fabricação mais antiga: {min(data_de_validade)}\n"
+            f"Empresa com mais produtos: {empresa}"
+        )
