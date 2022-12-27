87f72073a9f483dc561c65973e2a2e80742d0d4e
davi <davijluna@gmail.com>
2022-12-27 00:45:27 -0300

    fix: correção de espaços desnecessarios no arquivo complete_report.py

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -8,7 +8,7 @@ class CompleteReport(SimpleReport):
         report = super().generate(file)
         empresas_com_mais_produtos = [item["nome_da_empresa"] for item in file]
         estocados_por_empresa = Counter(empresas_com_mais_produtos)
-        report += "\n Produtos estocados por empresa:\n"
+        report += "\nProdutos estocados por empresa:\n"
         for inc, quantidades in estocados_por_empresa.items():
             report += f"- {inc}: {quantidades}\n"
 
