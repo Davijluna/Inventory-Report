d417a2f236edb686dc05bd2f85cfaa454d4cb5af
davi <davijluna@gmail.com>
2022-12-27 00:26:00 -0300

    feat: requisito 3 implementando versão completa do relatório

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,15 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, file):
+        report = super().generate(file)
+        empresas_com_mais_produtos = [item["nome_da_empresa"] for item in file]
+        estocados_por_empresa = Counter(empresas_com_mais_produtos)
+        report += "\n Produtos estocados por empresa: \n"
+        for inc, quantidades in estocados_por_empresa.items():
+            report += f"- {inc}: {quantidades} QUANTIDADE \n"
+
+        return report
