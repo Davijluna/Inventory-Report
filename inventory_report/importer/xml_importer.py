f8befb3ceef76591a3d3ed48c439436c41084973
davi <davijluna@gmail.com>
2022-12-29 19:29:48 -0300

    refactor: tentando refazer estruturação de arquivo xml

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,11 +1,18 @@
 from inventory_report.importer.importer import Importer
-import xmltodict
+import xml.etree.ElementTree as ET
+# import xmltodict as ET/
 
 
 class XmlImporter(Importer):
     @classmethod
     def import_data(cls, data: str):
         if data.endswith(".xml"):
-            with open(data) as XmlFile:
-                return xmltodict.parse(XmlFile.read())['dataset']['record']
+            # with open(data) as XmlFile:
+            #     return ET.parse(XmlFile.read())['dataset']['record']
+            with open(data, mode='r') as test:
+                root = ET.parse(test).getroot()
+                return [
+                    {line.tag: line.text for line in record}
+                    for record in root
+                ]
         raise ValueError("Arquivo inválido")
