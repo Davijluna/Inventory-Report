12b5b1e14047bbceca070e7723cf993474a7be68
davi <davijluna@gmail.com>
2022-12-28 21:44:02 -0300

    feat: implementando arquivo json_importer.py

---
+++ inventory_report/importer/json_importer.py
@@ -0,0 +1,11 @@
+from inventory_report.importer.importer import Importer
+import json
+
+
+class JsonImporter(Importer):
+    @classmethod
+    def import_data(cls, data: str):
+        if data.endswith(".json"):
+            with open(data) as jeisao:
+                return json.load(jeisao)
+        raise ValueError("Arquivo inválido")
