7d755cbccc611465c5e5a50a844ea54de51b9853
davi <davijluna@gmail.com>
2023-01-01 19:54:19 -0300

    test: implementando test de test_product_report.py

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    test = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        4467254944021,
+        "instrucao 1",
+    )
+    assert str(test) == (
+            f"O produto {test.nome_do_produto}"
+            f" fabricado em {test.data_de_fabricacao}"
+            f" por {test.nome_da_empresa} com validade"
+            f" até {test.data_de_validade}"
+            f" precisa ser armazenado {test.instrucoes_de_armazenamento}."
+        )
