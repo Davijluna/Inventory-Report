1af40d04997dc0413d56c90d46fd14dbe5045fd6
davi <davijluna@gmail.com>
2023-01-01 20:20:46 -0300

    refactor: trocando nome de variavel test para test_self

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -2,7 +2,7 @@ from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    test = Product(
+    test_self = Product(
         1,
         "Nicotine Polacrilex",
         "Target Corporation",
@@ -11,10 +11,10 @@ def test_relatorio_produto():
         4467254944021,
         "instrucao 1",
     )
-    assert str(test) == (
-            f"O produto {test.nome_do_produto}"
-            f" fabricado em {test.data_de_fabricacao}"
-            f" por {test.nome_da_empresa} com validade"
-            f" até {test.data_de_validade}"
-            f" precisa ser armazenado {test.instrucoes_de_armazenamento}."
+    assert str(test_self) == (
+            f"O produto {test_self.nome_do_produto}"
+            f" fabricado em {test_self.data_de_fabricacao}"
+            f" por {test_self.nome_da_empresa} com validade"
+            f" até {test_self.data_de_validade}"
+            f" precisa ser armazenado {test_self.instrucoes_de_armazenamento}."
         )
